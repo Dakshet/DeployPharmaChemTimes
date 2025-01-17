5d422d7138b6e9b9f75b3795fa78d3a2486016be
Dakshet <dakshghole@gmail.com>
2025-01-18 01:48:08 +0530

    Here update the code.

--- backend/index.js
+++ backend/index.js
@@ -61,13 +61,13 @@ app.use(prerender);
 
 
 // Explicit handling of preflight requests
-app.options('*', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', FRONTEND_URL);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization', "auth_token");
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.sendStatus(200); // No Content
-});
+// app.options('*', (req, res) => {
+//     res.setHeader('Access-Control-Allow-Origin', FRONTEND_URL);
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization', "auth_token");
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     res.sendStatus(200); // No Content
+// });
 
 
 // Middleware
